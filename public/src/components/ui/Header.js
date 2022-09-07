25bcb6fbf60e71db6c1e994e8cb61650194e0d7c
Hadiseh Khorasani <llhadisehll@gmail.com>
2022-09-07 21:01:22 +0430

    add folder (assets & animations )to src

---
+++ public/src/components/ui/Header.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+
+
+
+export default function Header(props){
+    return (
+        <AppBar>
+            <Toolbar>
+                Arc Development
+            </Toolbar>
+        </AppBar>
+
+    );
+}
